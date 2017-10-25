523e81720728e2ae3e9ea16548aea992f138f7e7
mcmorro1 <mcmorro1@cpcom102macm51.care.asu.edu>
2017-10-25 13:50:01 -0700

    Link text inputs to code. Set up test label.

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var Height_UserInput: UITextField!
+    
+    
+    @IBOutlet weak var Weight_UserInput: UITextField!
+    
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
+    var user_height: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Height_UserInput.delegate = self
+        
     }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        user_height = textField.text!
+        testLabel.text = user_height
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
