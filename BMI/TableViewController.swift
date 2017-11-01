f25bbd53da7ac3b269528b631a78e2f2b2c4a976
MariaM <mcmorro1@asu.edu>
2017-11-01 13:56:36 -0700

    Table View Controller Display Setup

--- BMI/TableViewController.swift
+++ BMI/TableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    let displayArray = [ "", user_height, user_weight, ""]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,23 +31,24 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 4
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         // Configure the cell...
-
+        cell.textLabel?.text = displayArray[indexPath.row]
+        
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
